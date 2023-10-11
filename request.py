83be910d2a009f82c1b1d0c07f11a43789400389
cyc9805 <cyc9805@gmail.com>
2023-10-11 17:37:04 +0900

    Refactor: zip file

--- request.py
+++ request.py
@@ -4,12 +4,12 @@ import time
 
 file_name = {"file_name":"2023-08-09T10:24:01.253651668_(796).webm"}
 # answer = requests.post("http://localhost:8081/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": "large-v2"})
-# answer = requests.post("http://localhost:8000/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
+answer = requests.post("http://localhost:8000/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
 time.sleep(1)
 answer = requests.post("http://localhost:8000/asr/2", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("IT")})
-# time.sleep(1)
-# answer = requests.post("http://localhost:8000/asr/3", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("IT")})
-# time.sleep(1)
-# answer = requests.post("http://localhost:8000/asr/4", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
+time.sleep(1)
+answer = requests.post("http://localhost:8000/asr/3", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("IT")})
+time.sleep(1)
+answer = requests.post("http://localhost:8000/asr/4", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
 print(json.loads(answer.text))
 

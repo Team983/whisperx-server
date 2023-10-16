6b53332444f06a8040bb3100c7bf7eba4d28a12a
cyc9805 <cyc9805@gmail.com>
2023-10-16 11:35:39 +0900

    Refactor

--- request.py
+++ request.py
@@ -1,9 +1,7 @@
 import requests
 import json
 import time
-# {“file_name”:“c19606c2-924c-43b7-8b64-093a7c60c374"}
-{'file_name': 'd84942af-0d92-41f9-8515-8966e258972a'}
-file_name = {"file_name":"d84942af-0d92-41f9-8515-8966e258972a'}
+file_name = {'file_name':'d84942af-0d92-41f9-8515-8966e258972a'}
 # answer = requests.post("http://localhost:8081/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": "large-v2"})
 answer = requests.post("http://team983.site:8000/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
 # time.sleep(1)

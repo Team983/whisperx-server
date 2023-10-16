d939583695402a0e155624e11c519bea97696e36
cyc9805 <cyc9805@gmail.com>
2023-10-16 23:48:12 +0900

    Refactor

--- request.py
+++ request.py
@@ -1,11 +1,11 @@
 import requests
 import json
 import time
-file_name = {"file_name":"035fa888-f83f-4567-87d4-ea9c06d96655"}
+file_name = {"file_name":"c43f05f7-821c-4336-92b3-d98007e0f7f0"}
 # answer = requests.post("http://localhost:8081/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": "large-v2"})
-answer = requests.post("https://team983.site/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
+# answer = requests.post("https://team983.site/asr/1", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("GENERAL")})
 # time.sleep(1)
-# answer = requests.post("http://localhost:8000/asr/2", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("IT")})
+answer = requests.post("http://localhost:8000/asr/2", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("IT")})
 # time.sleep(1)
 # answer = requests.post("http://localhost:8000/asr/3", json=json.dumps(file_name), headers={"serve_multiplexed_model_id": str("IT")})
 # time.sleep(1)
--- whisperx-deployment.py
+++ whisperx-deployment.py
@@ -94,9 +94,20 @@ class APIIngress:
             logger.info('Original: %s, Converted to m4a at: %s, Duration: %f', og_filepath, converted_filepath, duration)
 
             audio = whisperx.load_audio(converted_filepath)
-            model_id = await serve.get_multiplexed_model_id()
-            self.full_handle.get_model.remote(model_id)
+
+            logger.info('Received request:')
+            logger.info(request)
+
+            # Original code
+            model_id = serve.get_multiplexed_model_id()
+            await self.full_handle.get_model.remote(model_id)
             self.full_handle.transcribe_audio.remote(note_id, audio)
+
+            # Modified code
+            # model_id = serve.get_multiplexed_model_id()
+            # self.full_handle.options(multiplexed_model_id=)get_model.remote(model_id)
+            # self.full_handle.transcribe_audio.remote(note_id, audio)
+            ######################
             if os.path.exists(converted_filepath):
                 os.remove(converted_filepath)
             

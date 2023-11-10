dbc51c4ccf4780413b9d02be2ab8e81cc45b42bd
cyc9805 <cyc9805@gmail.com>
2023-11-11 02:53:36 +0900

    fix: download_s3 to async

--- services/s3_service.py
+++ services/s3_service.py
@@ -14,7 +14,7 @@ s3 = boto3.client(
     region_name=AWS_REGION
 )
 
-def download_file_from_s3(filepath: str):
+async def download_file_from_s3(filepath: str):
     with open(filepath, 'wb') as f:
         s3.download_fileobj(BUCKET_NAME, os.path.basename(filepath), f)
 
